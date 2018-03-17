f602bd947a1cb02ae7b129e7b7db8953ee03245d
egor-klementyev <x_dead_lock@outlook.com>
2018-03-17 14:28:33 +0300

    first version

---
+++ hostel-app/src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+class Item{
+    purchase: string;
+    done: boolean;
+    price: number;
+     
+    constructor(purchase: string, price: number) {
+  
+        this.purchase = purchase;
+        this.price = price;
+        this.done = false;
+    }
+}
+     
+@Component({
+    selector: 'hostel-app',
+    templateUrl: './templates/app.component.html',
+    // styleUrls: ['./templates/app.component.css']    
+})
+export class AppComponent { 
+    items: Item[] = 
+    [
+        { purchase: "Хлеб", done: false, price: 15.9 },
+        { purchase: "Масло", done: false, price: 60 },
+        { purchase: "Картофель", done: true, price: 22.6 },
+        { purchase: "Сыр", done: false, price:310 }
+    ];
+    addItem(text: string, price: number): void {
+         
+        if(text==null || text==undefined || text.trim()=="")
+            return;
+        if(price==null || price==undefined)
+            return;
+        this.items.push(new Item(text, price));
+    }
+}
